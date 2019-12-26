557c7a6ff7aa2bdad083050d177e3fb1dd9b5288
Guilherme Saturno <guilhermebsaturno@gmail.com>
2019-12-26 15:48:32 +0000

    Add GameLoop method to MainControl class

--- SnakeGame/MainControl.cs
+++ SnakeGame/MainControl.cs
@@ -1,11 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SnakeGame
 {
     public class MainControl
     {
-        // Game loop
+        private const double MS_PER_UPDATE = 5;
+
+        private Snake snake;
+
+        public void GameLoop()
+        {
+            double previous = DateTime.Now.Ticks;
+            double lag = 0l;
+
+            while (true)
+            {
+                double current = DateTime.Now.Ticks;
+                double elapsed = current - previous;
+                previous = current;
+                lag += elapsed;
+
+               //Input();
+
+                while (lag >= MS_PER_UPDATE)
+                {
+                    //Update();
+                    lag -= MS_PER_UPDATE;
+                }
+
+                //Render();
+            }
+        }
     }
 }
